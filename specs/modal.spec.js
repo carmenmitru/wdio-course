d6dbb778704c52edc5a8c1f4a9554627b45d25b8
Carmen M <carmen.mitru@gmail.com>
2019-12-15 13:18:14 +0200

    add starter bundle

--- specs/modal.spec.js
+++ specs/modal.spec.js
@@ -1,20 +1,13 @@
-
-describe('Modal', () => {
+describe("Modal", () => {
     beforeEach(() => {
-        browser.url("http://localhost:4200/#/modal")
-
-    })
-    it('render modal on click open', () => {
-        let modalHeader = "Profile update";
-        let modalBody = "Modal Content";
+        browser.url("./#/modal");
+    });
+    it("renders the modal on btn click", () => {
         let openBtn = browser.$("button");
         openBtn.click();
         let modal = browser.$("ngb-modal-window");
-        modal.waitForDisplayed(13500);
-        expect(modal.$("div.modal-header > h4").getText()).to.equal(modalHeader);
-        expect(modal.$("div.modal-body").getText()).to.equal(modalBody);
+        modal.waitForDisplayed(14000);
+        expect(modal.isDisplayed()).to.be.true;
     });
+
 });
-
-
-
