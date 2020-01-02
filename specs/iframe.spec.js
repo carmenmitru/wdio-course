ecd969c13739f094bd2ac0bd95e80be2a8c4b605
Mitru Carmen <carmen.mitru@gmail.com>
2020-01-02 19:04:08 +0200

    update tests

--- specs/iframe.spec.js
+++ specs/iframe.spec.js
@@ -11,10 +11,19 @@ describe("Iframe", () => {
         expect(header.getText()).to.equal("From parent to iframe");
     });
     it('should save screenshot of fullpage', () => {
-        browser.saveFullPageScreen('iframe', { /* some options*/ });
+        browser.saveFullPageScreen('iframe', {});
     });
 
     it('should compare successful with a baseline', () => {
-        expect(browser.checkScreen('iframe', { /* some options*/ })).to.equal(0);
+        expect(browser.checkScreen('iframe', {})).to.equal(0);
+    });
+
+    it('should save screenshot of navbar', () => {
+        browser.saveElement(browser.$('nav'), 'navbar', {});
+
+    });
+
+    it('should compare navabr successful with a baseline navbar', () => {
+        expect(browser.checkElement(browser.$('nav'), 'navbar', {})).to.equal(0);
     });
 });
